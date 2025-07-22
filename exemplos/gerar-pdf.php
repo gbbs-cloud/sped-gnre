7e8dd0fa9df9381ed58d720790d75f9507def980
João Marques <jotaveultra@gmail.com>
2025-07-22 17:52:38 -0300

    fix gerar pdf exemplo

--- exemplos/gerar-pdf.php
+++ exemplos/gerar-pdf.php
@@ -1,32 +1,37 @@
 <?php
 
+use Sped\Gnre\Render\Html;
+use Sped\Gnre\Render\Pdf;
+use Sped\Gnre\Sefaz\Guia;
+use Sped\Gnre\Sefaz\Lote;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$guia = new Sped\Gnre\Sefaz\Guia();
+$guia = new Guia();
 $guia->c01_UfFavorecida = 'SP';
-$guia->c02_receita = 1000099;
-$guia->c25_detalhamentoReceita = 10101010;
+$guia->c02_receita = '1000099';
+$guia->c25_detalhamentoReceita = '10101010';
 $guia->c26_produto = 'TESTE DE PROD';
 $guia->c27_tipoIdentificacaoEmitente = 1;
-$guia->c03_idContribuinteEmitente = 41819055000105;
-$guia->c28_tipoDocOrigem = 10;
-$guia->c04_docOrigem = 5656;
+$guia->c03_idContribuinteEmitente = '41819055000105';
+$guia->c28_tipoDocOrigem = '10';
+$guia->c04_docOrigem = '5656';
 $guia->c06_valorPrincipal = 10.99;
 $guia->c10_valorTotal = 12.52;
 $guia->c14_dataVencimento = '01/05/2015';
-$guia->c15_convenio = 546456;
+$guia->c15_convenio = '546456';
 $guia->c16_razaoSocialEmitente = 'GNRE PHP EMITENTE';
-$guia->c17_inscricaoEstadualEmitente = 56756;
+$guia->c17_inscricaoEstadualEmitente = '56756';
 $guia->c18_enderecoEmitente = 'Queens St';
-$guia->c19_municipioEmitente = 5300108;
+$guia->c19_municipioEmitente = '5300108';
 $guia->c20_ufEnderecoEmitente = 'DF';
 $guia->c21_cepEmitente = '08215917';
-$guia->c22_telefoneEmitente = 1199999999;
+$guia->c22_telefoneEmitente = '1199999999';
 $guia->c34_tipoIdentificacaoDestinatario = 1;
-$guia->c35_idContribuinteDestinatario = 86268158000162;
-$guia->c36_inscricaoEstadualDestinatario = 10809181;
+$guia->c35_idContribuinteDestinatario = '86268158000162';
+$guia->c36_inscricaoEstadualDestinatario = '10809181';
 $guia->c37_razaoSocialDestinatario = 'RAZAO SOCIAL GNRE PHP DESTINATARIO';
-$guia->c38_municipioDestinatario = 2702306;
+$guia->c38_municipioDestinatario = '2702306';
 $guia->c33_dataPagamento = '2015-11-30';
 $guia->retornoInformacoesComplementares = 'teste teste teste';
 $guia->retornoAtualizacaoMonetaria = 1.88;
@@ -36,15 +41,15 @@ $guia->retornoRepresentacaoNumerica = '11189298129120110000000018181810000000012
 $guia->retornoJuros = 2.78;
 $guia->retornoMulta = 3.55;
 $guia->mes = '05';
-$guia->ano = 2015;
-$guia->parcela = 2;
-$guia->periodo = 2014;
+$guia->ano = '2015';
+$guia->parcela = '2';
+$guia->periodo = '2014';
 
-$lote = new Sped\Gnre\Sefaz\Lote();
+$lote = new Lote();
 $lote->addGuia($guia);
 
-$html = new Sped\Gnre\Render\Html();
+$html = new Html();
 $html->create($lote);
 
-$pdf = new Sped\Gnre\Render\Pdf();
+$pdf = new Pdf();
 $pdf->create($html)->stream('gnre.pdf', ['Attachment' => 0]);
