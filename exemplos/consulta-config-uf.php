44ce4dc5bbf954ad3404b727f9b676d5a40e85a9
João Marques <jotaveultra@gmail.com>
2025-07-30 13:45:01 -0300

    ssl version (hardcode in the connection?)

--- exemplos/consulta-config-uf.php
+++ exemplos/consulta-config-uf.php
@@ -12,7 +12,7 @@ class MySetup extends Setup
 {
     public function getCertificatePemFile(): string
     {
-        return '';
+        return __DIR__ . '/../certs/private_key.pem';
     }
 
     public function getPrivateKey(): string
@@ -39,10 +39,16 @@ $config = new ConfigUf();
  * Qual ambiente será realizada a consulta
  */
 $config->setEnvironment(1);
+$config->utilizarAmbienteDeTeste(true);
 $config->setReceita(100099);
 $config->setEstado('PR');
 
 $webService = new Connection($minhaConfiguracao, $config->getHeaderSoap(), $config->toXml());
+$webService->addCurlOption([
+    CURLOPT_SSL_VERIFYHOST => 0,
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+]);
 
 $consulta = $webService->doRequest($config->soapAction());
 echo '<pre>';
